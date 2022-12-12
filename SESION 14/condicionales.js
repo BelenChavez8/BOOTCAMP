405035079bf80c95c965b0cce5de9ddf2011d90c
belen-ctc <belenchavezalarcon@gmail.com@gmail.com>
2022-12-12 10:35:00 -0500

    ARCHIVOS DE PROYECTOS

---
+++ SESION 14/condicionales.js
@@ -0,0 +1,13 @@
+var numero1=15;
+var numero2=10;
+
+if(numero1>numero2){
+console.log("El numero 1 si es mayor");    
+}
+
+else if(numero1==numero2){
+
+console.log("Ambos numeros son iguales");    
+}
+
+else{}
