4299313d3b8df1ff9bfad9fb485d21071bc31d7b
Belén Chávez Alarcón <120392769+BelenChavez8@users.noreply.github.com>
2022-12-19 11:25:20 -0500

    Add files via upload

---
+++ U2/SEMANA1/SESION26/JS/index.js
@@ -0,0 +1,52 @@
+var numero=5;
+while(numero<=10){
+    console.log("hola");
+    numero=numero+1      //numero=5+1=6   numero=6+1=7
+}
+//RETO 1
+let num=0 //INICIALIZANDO UNA VARIABLE O LET
+while(num<=100){
+
+    console.log(num);
+    num=num+2;  //ACTUALIZANDO A LOS PARES
+}
+//RETO 2
+let number=1
+while(number<=12){
+
+    console.log(number);
+    number=number+2;
+}
+//RETO 3
+let numeros=1
+while(numeros<=12){
+    
+    console.log(numeros);
+    numeros=numeros+1
+}
+//RETO 4: RECORRIENDO ARREGLOS
+var juegos =["minecraf","FIFA",50,25,10]// 5 ELEMENTOS
+
+let i=0//INICIALIZANDO
+while(i<=(juegos.length-1 ));
+  console.log(juegos[i]);
+  i=i+1
+
+let inicio=1;
+do{
+console.log(inicio);
+inicio=inicio+2;
+}while(inicio<=100 );
+
+
+
+let comienzo=10;
+do{
+    console.log(comienzo);
+    comienzo=comienzo+4;
+
+}while(comienzo <=50 );
+
+
+
+
---
+++ U2/SEMANA1/SESION27/JS/index.js
@@ -0,0 +1,51 @@
+
+function  resta(a,b){
+
+    return a-b;
+
+}
+
+console.log(resta(5,4));
+
+
+function suma(numero1,numero2){
+
+    return numero1+numero2;
+
+}
+
+console.log( suma(20,5) );
+
+function multiplicacion(a,b){
+      return a*b
+}
+console.log(multiplicacion(3,3));
+
+function division(a,b){
+       return a/b 
+}
+console.log(division(20,4));
+
+function suma(numero1,numero2,numero3,numero4){
+        return numero1+numero2+numero3+numero4
+}
+console.log(suma(23,12,8,9))
+
+function suma_resta(a,b,c){
+        return a+b-c
+}
+console.log(suma_resta(2,2,1))
+
+function ayuda(a,b){
+
+    if((a*b)>20){
+        return "LA MULTIPLICACIÓN SÍ ES MAYOR A 20";
+    }
+
+    else{
+        return "NO ES MAYOR A 20";
+    }
+
+}
+
+console.log(ayuda(5,8))
